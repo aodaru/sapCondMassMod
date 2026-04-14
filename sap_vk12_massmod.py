bb621cd62d4b6ac84ac1fc375c4729d814084703
Adal Garcia <aodarug@gmail.com>
2026-04-14 17:01:34 -0500

    se agrego el flow para OrgVent, CanDist,GrpArt. queda pendiente corregir el bucle ya que solo se ejecuta una vez

--- sap_vk12_massmod.py
+++ sap_vk12_massmod.py
@@ -17,10 +17,19 @@ class SapVk12MassMod:
         raise ValueError(f"Flujo desconocido: {flow_name}")
 
     def run_vk12_massmod(self, file_path):
-        data = pd.read_excel(file_path, sheet_name="Hoja1").astype(str)
+        # data = pd.read_excel(file_path, sheet_name="Hoja1").astype(str)
+        data = pd.read_excel(file_path, sheet_name="Hoja1", dtype=str)
         data.columns = data.columns.str.strip().str.upper().str.replace(" ", "_")
 
-        required_columns = ["MATERIAL", "UNIDAD_DE_MEDIDA", "IMPORTE", "TIPO_MODIFICACION"]
+        required_columns = [
+            "MATERIAL", 
+            "UNIDAD_DE_MEDIDA", 
+            "IMPORTE", 
+            "GRUPO_ARTICULO",
+            "ORG_VENTA",
+            "CAN_DISTR",
+            "TIPO_MODIFICACION"
+            ]
         missing_columns = [c for c in required_columns if c not in data.columns]
         if missing_columns:
             raise ValueError(f"Faltan columnas en el Excel: {', '.join(missing_columns)}")
@@ -39,6 +48,7 @@ class SapVk12MassMod:
 
     # Flujos modificacion masiva
     def _mat_orgvent_candistr(self, session, row, index):
+        print(f"Material: {row.MATERIAL}")
         session.findById("wnd[0]/tbar[0]/okcd").text = "vk12"
         session.findById("wnd[0]").sendVKey(0)
         time.sleep(1)
@@ -50,8 +60,8 @@ class SapVk12MassMod:
         session.findById("wnd[1]/usr/sub:SAPLV14A:0100/radRV130-SELKZ[1,0]").setFocus()
         session.findById("wnd[1]").sendVKey(0)
         time.sleep(1)
-        session.findById("wnd[0]/usr/ctxtF001").text = "1000"
-        session.findById("wnd[0]/usr/ctxtF002").text = "10"
+        session.findById("wnd[0]/usr/ctxtF001").text = row.ORG_VENTA
+        session.findById("wnd[0]/usr/ctxtF002").text = row.CAN_DISTR
         session.findById("wnd[0]/usr/ctxtF003-LOW").text = row.MATERIAL
         session.findById("wnd[0]/usr/ctxtF003-LOW").setFocus()
         session.findById("wnd[0]/usr/ctxtF003-LOW").caretPosition = 8
@@ -88,6 +98,42 @@ class SapVk12MassMod:
         session.findById("wnd[0]/tbar[0]/btn[3]").press()
         time.sleep(1)
         session.findById("wnd[0]/tbar[0]/btn[3]").press()
+
+    # ## Organización de ventas, canal de distribución, grupo de artículo
+    def _orgvent_candistr_gpoart(self, session, row, index):
+        session.findById("wnd[0]/tbar[0]/okcd").text = "vk12"
+        session.findById("wnd[0]").sendVKey(0)
+        time.sleep(1)
+        session.findById("wnd[0]/usr/ctxtRV13A-KSCHL").text = "Z004"
+        session.findById("wnd[0]/usr/ctxtRV13A-KSCHL").caretPosition = 4
+        session.findById("wnd[0]").sendVKey(0)
+        time.sleep(1)
+        session.findById("wnd[1]/usr/sub:SAPLV14A:0100/radRV130-SELKZ[6,0]").select()
+        session.findById("wnd[1]/usr/sub:SAPLV14A:0100/radRV130-SELKZ[6,0]").setFocus()
+        session.findById("wnd[1]").sendVKey(0)
+        time.sleep(1)
+        session.findById("wnd[0]/usr/ctxtF001").text = row.ORG_VENTA
+        session.findById("wnd[0]/usr/ctxtF002").text = row.CAN_DISTR
+        session.findById("wnd[0]/usr/ctxtF003-LOW").text = row.GRUPO_ARTICULO
+        session.findById("wnd[0]/usr/ctxtF003-LOW").setFocus()
+        session.findById("wnd[0]/usr/ctxtF003-LOW").caretPosition = 9
+        session.findById("wnd[0]/tbar[1]/btn[8]").press()
+        time.sleep(1)
+        gpo_mat_field = session.findById("wnd[0]/usr/tblSAPMV13ATCTRL_FAST_ENTRY/txtKONP-MATKL[2,0]")
+        existing_gpo_mat = gpo_mat_field.text
+        if existing_gpo_mat:
+            print(f"Fila {index}: grupo de material ya contiene datos: {existing_gpo_mat}")
+        else:
+            gpo_mat_field.text = row.GRUPO_ARTICULO
+    
+        session.findById("wnd[0]/usr/tblSAPMV13ATCTRL_FAST_ENTRY/txtKONP-KBETR[2,0]").text = "4"
+        session.findById("wnd[0]/usr/tblSAPMV13ATCTRL_FAST_ENTRY/txtKONP-KBETR[2,0]").setFocus
+        session.findById("wnd[0]/usr/tblSAPMV13ATCTRL_FAST_ENTRY/txtKONP-KBETR[2,0]").caretPosition = 16
+        session.findById("wnd[0]").sendVKey(0)
+        session.findById("wnd[0]/tbar[0]/btn[11]").press()
+        session.findById("wnd[0]/tbar[0]/btn[3]").press()
+        session.findById("wnd[0]/tbar[0]/btn[3]").press()
+
     
     # Fin - Flujos modificacion masiva
 
@@ -95,11 +141,12 @@ class SapVk12MassMod:
     def mat_orgvent_candistr(self, session, row, index):
         self._mat_orgvent_candistr(session, row, index)
 
+    def orgvent_candistr_gpoart(self, session, row, index):
+        pass
+
     def orgvent_candistr_sec_ramo_mat(self, session, row, index):
         pass
 
     def orgven_candist_sec_gpoart(self, session, row, index):
         pass
 
-    def _orgvent_candistr_gpoart(self, session, row, index):
-        pass
